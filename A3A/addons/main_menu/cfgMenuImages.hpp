c9239d5010068fc635bbb75c2117344c90e1bc1f
stutpip123 <77023662+stutpip123@users.noreply.github.com>
2024-10-03 20:37:49 +0100

    Squashed commit of the following:
    commit e65a944502135f68acc162d7b13c75874c6834c2
    Author: MaxxLite <162753200+UAC-MaxxLite@users.noreply.github.com>
    Date:   Thu Oct 3 15:55:47 2024 +0300
    
        future proofing
    
    commit e87cc82f9e3835e172b384e69d5b8a507efb6984
    Author: MaxxLite <162753200+UAC-MaxxLite@users.noreply.github.com>
    Date:   Wed Oct 2 10:49:42 2024 +0300
    
        removed unused fnc
    
    commit 4ff945976f4630a4a8634db900b39ab98c59055a
    Author: MaxxLite <162753200+UAC-MaxxLite@users.noreply.github.com>
    Date:   Wed Oct 2 00:22:33 2024 +0300
    
        documentation
    
    commit be1df125e91cd2179c82788722684addda646c32
    Author: MaxxLite <162753200+UAC-MaxxLite@users.noreply.github.com>
    Date:   Wed Oct 2 00:17:25 2024 +0300
    
        added images
    
    commit 6d6a1465d5c657a962760245022fda07f29dd506
    Author: MaxxLite <162753200+UAC-MaxxLite@users.noreply.github.com>
    Date:   Wed Oct 2 00:17:12 2024 +0300
    
        bug fixes
    
    commit 1fe14b25ac8f43b65320105f1d45603370cd934b
    Author: MaxxLite <162753200+UAC-MaxxLite@users.noreply.github.com>
    Date:   Tue Oct 1 22:11:38 2024 +0300
    
        base changes

---
+++ A3A/addons/main_menu/cfgMenuImages.hpp
@@ -0,0 +1,141 @@
+    /*
+        the "path" value expects a string pointing to an .jpg image file.
+
+        the "name" value is what gets used in the cba settings.
+    */
+
+    class intro_empty
+    {
+        path = "None";
+        name = "None";
+    };
+    class intro_bwa
+    {
+        path = QPATHTOFOLDER(data\backgrounds\images\bwa_co.jpg);
+        name = "BWA";
+    };
+    class intro_canada
+    {
+        path = QPATHTOFOLDER(data\backgrounds\images\canada_co.jpg);
+        name = "Canada";
+    };
+    class intro_cars
+    {
+        path = QPATHTOFOLDER(data\backgrounds\images\Cars_co.jpg);
+        name = "Cars";
+    };
+    class intro_clone_wars
+    {
+        path = QPATHTOFOLDER(data\backgrounds\images\clone_wars_co.jpg);
+        name = "Clone Wars";
+    };
+    class intro_ffaa
+    {
+        path = QPATHTOFOLDER(data\backgrounds\images\ffaa_co.jpg);
+        name = "FFAA";
+    };
+    class intro_italy
+    {
+        path = QPATHTOFOLDER(data\backgrounds\images\italy_co.jpg);
+        name = "Italy";
+    };
+    class intro_maps
+    {
+        path = QPATHTOFOLDER(data\backgrounds\images\Maps_co.jpg);
+        name = "Maps";
+    };
+    class intro_modsets
+    {
+        path = QPATHTOFOLDER(data\backgrounds\images\Modsets_co.jpg);
+        name = "Modsets";
+    };
+    class intro_a3au
+    {
+        path = QPATHTOFOLDER(data\backgrounds\images\a3au_co.jpg);
+        name = "Arma 3 Antistasi Ultimate";
+    };
+    class intro_racs
+    {
+        path = QPATHTOFOLDER(data\backgrounds\images\racs_co.jpg);
+        name = "Racs";
+    };
+    class intro_chase
+    {
+        path = QPATHTOFOLDER(data\backgrounds\images\chase_co.jpg);
+        name = "chase";
+    };
+    class intro_russia
+    {
+        path = QPATHTOFOLDER(data\backgrounds\images\russia_co.jpg);
+        name = "Russia";
+    };
+    class intro_selfie
+    {
+        path = QPATHTOFOLDER(data\backgrounds\images\selfie_co.jpg);
+        name = "#Selfie!";
+    };
+    class intro_spearhead
+    {
+        path = QPATHTOFOLDER(data\backgrounds\images\spearhead_1944_co.jpg);
+        name = "Spearhead 1944";
+    };
+    class intro_stealth
+    {
+        path = QPATHTOFOLDER(data\backgrounds\images\Stealth_co.jpg);
+        name = "Stealth";
+    };
+    class intro_sunset
+    {
+        path = QPATHTOFOLDER(data\backgrounds\images\sunset_co.jpg);
+        name = "Sunset";
+    };
+    class intro_sweden
+    {
+        path = QPATHTOFOLDER(data\backgrounds\images\sweden_co.jpg);
+        name = "Sweden";
+    };
+    class intro_transport
+    {
+        path = QPATHTOFOLDER(data\backgrounds\images\transport_co.jpg);
+        name = "Transport";
+    };
+    class intro_turkey
+    {
+        path = QPATHTOFOLDER(data\backgrounds\images\turkey_co.jpg);
+        name = "Turkey";
+    };
+    class intro_us_army
+    {
+        path = QPATHTOFOLDER(data\backgrounds\images\us_army_co.jpg);
+        name = "US Army";
+    };
+    class intro_usmc
+    {
+        path = QPATHTOFOLDER(data\backgrounds\images\usmc_co.jpg);
+        name = "USMC";
+    };
+    class intro_vietnam
+    {
+        path = QPATHTOFOLDER(data\backgrounds\images\vietnam_co.jpg);
+        name = "Vietnam";
+    };
+    class intro_zombies
+    {
+        path = QPATHTOFOLDER(data\backgrounds\images\Zombies_co.jpg);
+        name = "Zombies";
+    };
+    class intro_hrloss
+    {
+        path = QPATHTOFOLDER(data\backgrounds\images\HRLoss_co.jpg);
+        name = "Rebellion Failed";
+    };
+    class intro_economicloss
+    {
+        path = QPATHTOFOLDER(data\backgrounds\images\economicLoss_co.jpg);
+        name = "Economic Ruin";
+    };
+    class intro_poploss
+    {
+        path = QPATHTOFOLDER(data\backgrounds\images\popLoss_co.jpg);
+        name = "Population Death";
+    };
