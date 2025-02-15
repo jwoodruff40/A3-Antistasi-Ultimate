bac6ff94ff1a7718cbc826b2322af1b50a385a1f
jwoodruff40 <22091381+jwoodruff40@users.noreply.github.com>
2025-02-15 14:24:41 -0500

    Vanilla, VN, and RHS Faction Flag and Logo Fixes

--- A3A/addons/core/Templates/Templates/Misc/templates.hpp
+++ A3A/addons/core/Templates/Templates/Misc/templates.hpp
@@ -4,8 +4,8 @@
         basepath = QPATHTOFOLDER(Templates\Templates\Misc);
         priority = 0;
         side = "Civ";
-        logo = "a3\ui_f\data\logos\arma3_white_ca.paa";
-        flagTexture = "a3\ui_f\data\logos\arma3_white_ca.paa";
+        logo = "a3\ui_f\data\logos\arma3_expansion_ca.paa";
+        flagTexture = "a3\data_f\flags\flag_white_co.paa";
         name = "No Civs";
         file = "Vanilla_Civ_Empty";
         description = "All civilian life was wiped out. (This is an experimental template, disabling traffic and all civ presence)";
